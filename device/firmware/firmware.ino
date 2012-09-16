a3946db9ad483af07e7d32193f0eeaf3aebc6a8c
Mikhail Yurasov <me@yurasov.me>
2012-09-16 05:36:14 -0700

    1.0

--- device/firmware/firmware.ino
+++ device/firmware/firmware.ino
@@ -1,6 +1,7 @@
 /**
  *  HandsOff
  *  (c) Mikhail Yurasov, 2012
+ *  @version 1.0
  */
 
 // output pins
@@ -15,10 +16,31 @@
 #define ON 1
 #define OFF 0
 
-// led constants
+// led mode
 #define R PIN_LED_R
 #define Y PIN_LED_Y
 #define G PIN_LED_G
+
+ // device connection status
+ #define DEVICE_ERROR -1
+ #define DEVICE_DISCONNECTED 0
+ #define DEVICE_WAITING 1
+ #define DEVICE_CONNECTED 2
+ #define DEVICE_WRONG 3
+
+ // macroses
+#define LOBYTE(x) ((char*)(&(x)))[0]
+#define HIBYTE(x) ((char*)(&(x)))[1]
+
+//
+
+#include <Max3421e.h>
+#include <Usb.h>
+
+//
+
+MAX3421E Max;
+USB Usb;
 
 /**
  * Init
@@ -33,36 +55,140 @@ void setup() {
   pinMode(PIN_BUZZER, OUTPUT);
 
   // set fuel pump off
-  set_pump(OFF);
+  setPump(OFF);
 
   // set buzzer off
-  set_buzzer(OFF);
+  setBuzzer(OFF);
+
+  // init USB
+  delay(100); // required to stabilize power
+  Max.powerOn(); // power on USB controller
+  delay(200); // wait for devices
+
+  // open searl port
+  // Serial.begin(9600);
 }
+
+ char pumpState = OFF;
 
 /**
  * Wait for something to happen
  */
 void loop() {
-  test_peripherals();
+
+  char deviceStatus;
+
+  deviceStatus = getDeviceStatus();
+
+  if (pumpState == OFF) {
+    if (deviceStatus == DEVICE_CONNECTED) {
+      setLed(G);
+      pumpState = ON;
+    } else if (deviceStatus == DEVICE_WRONG) {
+      setLed(Y);
+      pumpState = OFF;
+    } else if (deviceStatus == DEVICE_DISCONNECTED) {
+      setLed(R);
+      pumpState = OFF;
+    } else if (deviceStatus == DEVICE_ERROR) {
+      setLed(OFF);
+      pumpState = OFF;
+    }
+  } else { // pump is ON
+    if (deviceStatus != DEVICE_CONNECTED) {
+
+      // device disconnected while car is probably running
+
+      long millisStart = millis();
+
+      // check for device
+      while (millis() - millisStart < 5000) { // 5 seconds
+        //delay(100);
+        beep();
+        deviceStatus = getDeviceStatus();
+
+
+        // device is connexcted back
+        if (deviceStatus == DEVICE_CONNECTED) {
+          return;
+        }
+      }
+
+      pumpState = OFF;
+    }
+  }
+
+  setPump(pumpState);
+  delay(200);
+}
+
+/**
+ * Get device connection status
+ */
+char getDeviceStatus() {
+  Max.Task();
+  Usb.Task();
+
+  if (Usb.getUsbTaskState() == USB_STATE_CONFIGURING) {
+    Usb.setConf(1, 0, 1);
+    Usb.setUsbTaskState(USB_STATE_RUNNING);
+
+    return DEVICE_WAITING;
+  } else if (Usb.getUsbTaskState() == USB_STATE_RUNNING) {
+
+    // get descriptior
+
+    USB_DEVICE_DESCRIPTOR buf;
+    byte rCode;
+
+    rCode = Usb.getDevDescr(1, 0, 0x12, (char *) &buf);
+    
+    if (rCode) {
+      return DEVICE_ERROR; // can't get device descriptor
+    }
+
+    if (buf.iSerialNumber == 0) {
+      return DEVICE_WRONG; // device doesn't have a serial number
+    }
+
+    // get SN
+
+    char serialNumber[64];
+    rCode = getUsbString(1, buf.iSerialNumber, serialNumber);
+
+    if (rCode) {
+      return DEVICE_ERROR; // can't read SN
+    }
+
+    // compare SN
+
+    if (strcmp(serialNumber, "9101600073078b0a25e694ffeb69d656bfbdb869") == 0) {
+      return DEVICE_CONNECTED;
+    } else {
+      return DEVICE_WRONG;
+    }
+  }
+
+  return DEVICE_DISCONNECTED;
 }
 
 /**
  * Test peripherals
  */
-void test_peripherals() {
-  set_led(R); delay(1000);
-  set_led(Y); delay(1000);
-  set_led(G); delay(1000);
+void testPeripherals() {
+  setLed(R); delay(1000);
+  setLed(Y); delay(1000);
+  setLed(G); delay(1000);
 
   beep();
 
-  set_pump(ON); delay(1000);
-  set_pump(OFF);
+  setPump(ON); delay(1000);
+  setPump(OFF);
 
-  set_buzzer(ON); delay(100);
-  set_buzzer(OFF); delay(75);
-  set_buzzer(ON); delay(100);
-  set_buzzer(OFF);
+  setBuzzer(ON); delay(100);
+  setBuzzer(OFF); delay(75);
+  setBuzzer(ON); delay(100);
+  setBuzzer(OFF);
 
   delay(1000);
 }
@@ -74,26 +200,26 @@ void beep() {
   tone(PIN_SPK, 1200, 333);
   delay(350);
   tone(PIN_SPK, 1200, 333);
-  delay(1000 - 350);
+  delay(777 - 350);
 }
 
 /**
  * Switch fuel pump on/off
  */
-void set_pump(int state) {
+void setPump(char state) {
   digitalWrite(PIN_PUMP, state ? HIGH : LOW);
 }
 /**
  * Switch buzzer on/off
  */
-void set_buzzer(int state) {
+void setBuzzer(char state) {
   digitalWrite(PIN_BUZZER, state ? LOW : HIGH);
 }
 
 /**
- * Change led
+ * Set active LED or turn them off
  */
-void set_led(int led) {
+void setLed(char led) {
   digitalWrite(PIN_LED_R, LOW);
   digitalWrite(PIN_LED_Y, LOW);
   digitalWrite(PIN_LED_G, LOW);
@@ -101,4 +227,65 @@ void set_led(int led) {
   if (led) {
     digitalWrite(led, HIGH);
   }
+}
+
+/**
+ * Get string from USB device descriptor
+ */
+byte getUsbString(byte deviceAddr, byte index, char * sn)
+{
+  char buf[256];
+  
+  byte rCode;
+  byte length;
+  byte i;
+  unsigned int langId;
+
+  // get language table length
+
+  rCode = Usb.getStrDescr(deviceAddr, 0, 1, 0, 0, buf);  
+
+  if(rCode) {
+    return rCode; // error retrieving LangID table length
+  }
+
+  length = buf[0]; // length is the first byte
+
+  // get language table
+
+  rCode = Usb.getStrDescr( deviceAddr, 0, length, 0, 0, buf );
+
+  if (rCode) {
+    return rCode; // error retrieving LangID table
+  }
+
+  HIBYTE(langId) = buf[3]; // get first langId
+  LOBYTE(langId) = buf[2];
+
+  rCode = Usb.getStrDescr(deviceAddr, 0, 1, index, langId, buf);
+
+  if(rCode) {
+    return rCode; // error retrieving string length
+  }
+
+  length = buf[0];
+
+  // get string
+
+  rCode = Usb.getStrDescr( deviceAddr, 0, length, index, langId, buf );
+
+  if (rCode) {
+    return rCode; // error retrieving string
+  }
+
+  byte sni = 0;
+
+  for (i = 2; i < length; i += 2) {
+    sn[sni] = buf[i];
+    sni++;
+  }
+
+  sn[sni] = 0; // terminate string
+
+  return 0;
 }
