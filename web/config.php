2fd6e03f9de5b3c6be4f714bca42033063bb0410
Mikhail Yurasov <me@yurasov.me>
2012-09-18 08:37:13 -0700

    Web

---
+++ web/config.php
@@ -0,0 +1,18 @@
+<!-- 
+Licensed by AT&T under 'Software Development Kit Tools Agreement.'2012
+TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION: http://developer.att.com/sdk_agreement/
+Copyright 2012 AT&T Intellectual Property. All rights reserved. http://developer.att.com
+For more information contact developer.support@att.com
+-->
+
+<?php
+    $api_key = "34ac03e335ffb194187c8b54e34ea12d";
+    $secret_key = "5da00886723c57e7";
+    $short_code = "29156087";
+    $short_code2 = "";
+    $FQDN = "https://api.att.com";
+    $oauth_file = "/Projects/HandsOff/web/oauthtoken.php";
+    $scope = "SMS";
+    $default_address = "425-615-5870";
+    $default_smsMsg = "test message";
+?>
